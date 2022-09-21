d45474eeb85dd33e777efa95094b72afec1fb3ed
st107030 <113540752+st107030@users.noreply.github.com>
2022-09-21 21:26:42 +0300

    Delete 2022.09.12-Homework-1 directory

--- 2022.09.12-Homework-1/Task10/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{//у меня не прогрузилась альтернатвная 10, как прогрузится - доделаю задание
-	int a, b;
-	cin >> a;
-	cin >> b;
-	return 0;
-}
