41f4847c7fae6d1e090d95d2fc4778788ba1eb31
st107030 <113540752+st107030@users.noreply.github.com>
2022-10-01 11:39:47 +0300

    Delete 2022.09.12-Homework-1 directory

--- 2022.09.12-Homework-1/Task1/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main(int argc, char* argv[])
-{
-	int n = 0;
-	std::cin >> n;
-	std::cout << "The next number for number " << n << " is " << n + 1 << "." << std::endl;
-	std::cout << "The previous number for number " << n << " is " << n - 1 << "." << std::endl;
-	return EXIT_SUCCESS;
-}
--- 2022.09.12-Homework-1/Task5/Source.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main(int argc, char* argv[])
-{
-	int n = 0;
-	std::cin >> n;
-	std::cout << (n % 10 + n / 10 % 10 + n / 100);
-	return EXIT_SUCCESS;
-}
