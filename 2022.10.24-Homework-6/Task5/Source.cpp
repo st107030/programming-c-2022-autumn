77db0df658b54c7605fe6967581d8e696e2a498d
st107030 <st107030@student.spbu.ru>
2022-11-20 19:35:24 +0300

    Где пираты берут крюки? В секонд-хенде

--- 2022.10.24-Homework-6/Task5/Source.cpp
+++ 2022.10.24-Homework-6/Task5/Source.cpp
@@ -5,22 +5,40 @@ int main(int argc, char* argv[])
 	int n = 0;
 	std::cin >> n;
 	int a[100000]{ 0 };
-	int min_num = 57748383;
 	int minc = 0;
+	int help = 0;
 	for (int i = 0; i < n; i++)
 	{
 		std::cin >> a[i];
-		if (a[i] < min_num)
+		if (a[i] < a[minc])
 		{
-			min_num = a[i];
 			minc = i;
 		}
 	}
-	for (int i = minc; i < n; i++)
+	if (minc != 0)
 	{
-		std::cout << a[i] << ' ';
+		for (int i = 0; i < minc / 2; i++)
+		{
+			help = a[i];
+			a[i] = a[minc - i - 1];
+			a[minc - i - 1] = help;
+		}
+		int x = minc;
+		int y = n - 1;
+		for (int i = 0; i < (y - x + 1) / 2; ++i)
+		{
+			help = a[x + i];
+			a[x + i] = a[y - i];
+			a[y - i] = help;
+		}
+		for (int i = 0; i < n / 2; i++)
+		{
+			help = a[i];
+			a[i] = a[n - i - 1];
+			a[n - i - 1] = help;
+		}
 	}
-	for (int i = 0; i < minc; i++)
+	for (int i = 0; i < n; i++)
 	{
 		std::cout << a[i] << ' ';
 	}
