1877fefb785d76aed2f73a95c61145e683f5a4c6
st107030 <st107030@student.spbu.ru>
2022-11-28 21:33:57 +0300

    А муж не вернулся

---
+++ 2022.11.14-Homework-7/Task4/Source.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	int n = 0; //строки
+	std::cin >> n;
+	int m = 0; //столбцы
+	std::cin >> m;
+	//создание самого массива на нужное количество строк
+	int** arr = (int**)malloc(n * sizeof(int*));
+	//в каждой строке нужное кол-во столбцов
+	for (int i = 0; i < n; ++i)
+	{
+		*(arr + i) = (int*)malloc(m * sizeof(int));
+	}
+	int sum = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			std::cin >> arr[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < (m / 2); ++j)
+		{
+			int temp = arr[i][j];
+			arr[i][j] = arr[i][m - j - 1];
+			arr[i][m - j - 1] = temp;
+		}
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			std::cout << arr[i][j] << ' ';
+			if (j == m - 1)
+			{
+				std::cout << std::endl;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		free(*(arr + i));
+	}
+	free(arr);
+	return EXIT_SUCCESS;
+}
