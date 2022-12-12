488e62aff6185d536965c6c72f38734b8ca68e3b
st107030 <st107030@student.spbu.ru>
2022-12-12 14:47:29 +0300

    Играйте в нормальные шахматы :(

--- 2022.12.12-Test-2/Task1/Source.cpp
+++ 2022.12.12-Test-2/Task1/Source.cpp
@@ -29,11 +29,11 @@ int main(int argc, char* argv[])
 
 int Mag(int num, int n, int k, int* xb, int* yb, int* ans) 
 {
-    int strike;
+    bool strike;
     for (int y = (num ? num + 1: 1); y <= n; y++)
         for (int x = 1; x <= n; x++) 
         {
-            strike = 0;
+            strike = true;
             for (int i = 0; i < num; i++) 
             {
                 int dist1 = (yb[i] - y > 0 ? yb[i] - y: y - yb[i]);
@@ -41,22 +41,24 @@ int Mag(int num, int n, int k, int* xb, int* yb, int* ans)
                 if ((dist1 == 0) || (dist2 == 0) || (dist1 == dist2) || (dist1 * dist2 == 2))
                 //1 усл: не тот ли столбец; 2: не та ли строка; 3: не та ли диагональ; 4: не бьёт ли как конь
                 {
-                    strike += 1; 
+                    strike = false; 
+                    break;
                 }
             }
-            if ((strike == 0) && (num < k - 1)) 
+            if ((strike == true) && (num < k - 1)) 
             {
                 xb[num] = x;      //ставим магараджу, поле свободное
                 yb[num] = y;
-                //std::cout << xb[num] << ' ' << yb[num] << ' ' << *ans << std::endl;
+                std::cout << xb[num] << ' ' << yb[num] << ' ' << *ans << std::endl;
                 Mag(num + 1, n, k, xb, yb, ans);
             }
             else
             {
                 if (num == k - 1)
                 {
-                    ++ *ans;
+                    ++*ans;
                 }
+
             }
         }
     return *ans;
